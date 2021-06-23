183741950fdb8c808a4bc446ae8157f9f27b8c3a
Ncesecrede <francescopm95@gmail.com>
2021-06-23 15:39:09 +0100

    Human Code

---
+++ script.js
@@ -0,0 +1,23 @@
+// Gli amici ti hanno appena invitato a passare con
+//  loro il sabato a mare, unirsi alla comitiva
+
+const festa = [];
+
+let invitoFesta = prompt("vuoi venire alla festa?");
+
+//Festa al mare
+//Invito tramite SMS
+//Accetto / Rifiuto
+
+//Se accetto entro nel gruppo della festa
+//Chiedo indicazione per come arrivare
+
+//Raggiungo la macchina nel parcheggio
+//Apro le porte della macchina
+//Metto in moto e raggiungo il posto indicato
+
+//Se non accetto, avviso che non ci sono
+
+//funzione invioSMS (textSms, destinatario){
+    
+//}
